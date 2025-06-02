7e75960bd13b42a89a13e9e1075ae4ec2c18492c
trevorbagels <therealangrybagel@gmail.com>
2025-06-02 01:17:21 -0700

    fixed month keys

--- dev/event_modules/police_incidents.py
+++ dev/event_modules/police_incidents.py
@@ -27,7 +27,7 @@ class Events(events.Events):
 			incident_type = title[0].strip()
 			locationDesc = title[1].strip()
 			t = x1.find("span").text.split(" ") #time May 18, 2025, 10:31:30 AM
-			months = {"January": 1, "Feburary": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
+			months = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
 			month = months[t[0]]
 			day = int(t[1].split(",")[0])
 			year = int(t[2].split(",")[0])
